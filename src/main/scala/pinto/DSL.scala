75a3f609c2f5279aa4c485b380148c3cb3eb659e
Roberto Bonvallet <roberto@mez.cl>
2019-02-10 23:38:27 -0300

    Playing with rectangular conectors

--- src/main/scala/pinto/DSL.scala
+++ src/main/scala/pinto/DSL.scala
@@ -2,17 +2,27 @@ package pintoz
 
 object DSL {
 
+  type Length = squants.Length
+  type Angle  = squants.Angle
+
   implicit class PointOps(val p: Point) extends AnyVal {
+
     def --(q: Point) = Path(p, LineTo(q))
+    def --+(dp: Point) = p -- (p + dp)
+
     def -|(q: Point) =
       if (p.y == q.y || p.x == q.x) Path(p, LineTo(q))
       else                          Path(p, LineTo(Point.corner(q, p)), LineTo(q))
+    def -|+(dp: Point) = p -| (p + dp)
+
+
     def |-(q: Point) =
       if (p.y == q.y || p.x == q.x) Path(p, LineTo(q))
       else                          Path(p, LineTo(Point.corner(p, q)), LineTo(q))
+    def |-+(dp: Point) = p |- (p + dp)
   }
 
-  implicit class PolylineOps(val pl: Polyline) extends AnyVal {
+  implicit class PathOps(val path: Path) extends AnyVal {
 
   }
 
---
+++ src/test/scala/pinto/Example.scala
@@ -0,0 +1,27 @@
+package pintoz
+
+import org.scalatest.{FunSpec, Matchers}
+
+class Example extends FunSpec with Matchers {
+
+  describe("DSL") {
+    it("does stuff") {
+
+      import pintoz.{Point => P}
+      import squants.space.LengthConversions._
+      import DSL._
+
+      implicit val gridSize = 0.5.mm
+
+      (P(1, 1) |- P(3, 4)) shouldBe Path(
+        P(1, 1),
+        LineTo(P(1, 4)),
+        LineTo(P(3, 4)),
+      )
+
+
+    }
+  }
+
+
+}
